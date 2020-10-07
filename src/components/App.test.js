518e1fd1aeca97eee9feb7084ef8162bb6563a75
RikBala <rik280491@googlemail.com>
2020-10-07 17:36:45 +0100

    test gift component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React from "react";
 // stops tests from indirectly asserting on behaviour of child components
-import { shallow } from 'enzyme';
-import App from './App';
-import '../setupTests';
+import { shallow } from "enzyme";
+import App from "./App";
+import "../setupTests";
 
-const app = shallow(<App />);
+describe("App", () => {
+	const app = shallow(<App />);
 
-it("renders correctly", () => {
-	expect(app).toMatchSnapshot();
+	it("renders correctly", () => {
+		expect(app).toMatchSnapshot();
+	});
+
+	it("initialises the `state` with an empty list of gifts", () => {
+		expect(app.state().gifts).toEqual([]);
+	});
+
+	// beforeEach here to avoid code duplication (DRY)
+	// afterEach to cleanup, avoids test pollution.
+	describe("when clicking the `add gift` button", () => {
+		beforeEach(() => {
+			app.find(".btn-add").simulate("click");
+		});
+
+		afterEach(() => {
+			app.setState({ gifts: [] });
+		});
+
+		it("adds a new gift to `state`", () => {
+			expect(app.state().gifts).toEqual([{ id: 1 }]);
+		});
+
+		it("adds a new gift to the rendered list", () => {
+			expect(app.find(".gift-list").children().length).toEqual(1);
+		});
+
+		it("creates a Gift component", () => {
+			expect(app.find("Gift").exists()).toBe(true);
+		});
+	});
 });
-
-it("initialises the `state` with an empty list of gifts", () => {
-	expect(app.state().gifts).toEqual([]);
-});
-
-it("adds a new gift to `state` when clicking the `add gift` button", () => {
-	app.find(".btn-add").simulate("click");
-
-	expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
-
-it("adds a new gift to the rendered list when clicking the `add gift` button", () => {
-    app.find('.btn-add').simulate('click')
-
-    expect(app.find('.gift-list').children().length).toEqual(2)
-})
-
